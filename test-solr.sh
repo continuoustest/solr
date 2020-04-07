537d1813692e0757a30424d9554fb7bfd5f3ac26
Christophe GARCIA <christophe.garcia@continuous.team>
2020-04-07 16:31:03 +0000

    add errors

--- test-solr.sh
+++ test-solr.sh
@@ -13,6 +13,7 @@ then
     echo "ping works"
 else
     echo "ping failed"
+    exit 1
 fi
 
 solr_status=$(curl -s -o -I -w "%{http_code}"  -X GET http://solr:8983/solr/admin/cores -d "action=STATUS" -d "core=$CPHP_SOLR_CORE_NAME")
@@ -24,6 +25,8 @@ then
     echo "status works"
 else
     echo "status failed"
+    exit 1
 fi
 
 echo "status : $solr_status | ping: $solr_ping"
+exit 0
