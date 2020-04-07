d99c5a6c6a8d19de11cfb0cc936984db132dc4db
Christophe GARCIA <christophe.garcia@continuous.team>
2020-04-07 15:28:10 +0000

    config import test

--- test-solr.sh
+++ test-solr.sh
@@ -2,11 +2,20 @@
 
 solr_ping=$(curl -s -o /dev/null -I -w "%{http_code}"  -X GET http://solr:8983/solr/${CPHP_SOLR_CORE_NAME}/admin/ping)
 
-solr_status=$(curl -s -o /dev/null -I -w "%{http_code}"  -X GET http://solr:8983/solr/admin/cores -d "action=STATUS" -d "core=$CPHP_SOLR_CORE_NAME")
-
-if [ $solr_ping -eq 200 ] && [ $solr_status -eq 200];
+if [ $solr_ping -eq 200 ];
 then
-    exit 0
+    echo "ping works"
+else
+    echo "ping failed"
 fi
 
-exit 1
+solr_status=$(curl -s -o -I -w "%{http_code}"  -X GET http://solr:8983/solr/admin/cores -d "action=STATUS" -d "core=$CPHP_SOLR_CORE_NAME")
+
+if [ $solr_status -eq 200 ];
+then
+    echo "status works"
+else
+    echo "status failed"
+fi
+
+echo "status : $solr_status | ping: $solr_ping"
